1d3261fcf305591e0454628329c7ae52ad28a132
cyl <chenyilin0760@gmail.com>
2023-10-18 17:39:07 +0800

    update ignored files

---
+++ input/utils/custom_scheduler.py
@@ -0,0 +1,131 @@
+import warnings
+from torch.optim.lr_scheduler import _LRScheduler
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+
+
+class ShopeeScheduler(_LRScheduler):
+    def __init__(self, optimizer, lr_start=5e-6, lr_max=1e-5,
+                 lr_min=1e-6, lr_ramp_ep=5, lr_sus_ep=0, lr_decay=0.8,
+                 last_epoch=-1):
+        self.lr_start = lr_start
+        self.lr_max = lr_max
+        self.lr_min = lr_min
+        self.lr_ramp_ep = lr_ramp_ep
+        self.lr_sus_ep = lr_sus_ep
+        self.lr_decay = lr_decay
+        super(ShopeeScheduler, self).__init__(optimizer, last_epoch)
+        
+    def get_lr(self):
+        if not self._get_lr_called_within_step:
+            warnings.warn("To get the last learning rate computed by the scheduler, "
+                          "please use `get_last_lr()`.", UserWarning)
+        
+        if self.last_epoch == 0:
+            self.last_epoch += 1
+            return [self.lr_start for _ in self.optimizer.param_groups]
+        
+        lr = self._compute_lr_from_epoch()
+        self.last_epoch += 1
+        
+        return [lr for _ in self.optimizer.param_groups]
+    
+    def _get_closed_form_lr(self):
+        return self.base_lrs
+    
+    def _compute_lr_from_epoch(self):
+        if self.last_epoch < self.lr_ramp_ep:
+            lr = ((self.lr_max - self.lr_start) / 
+                  self.lr_ramp_ep * self.last_epoch + 
+                  self.lr_start)
+        
+        elif self.last_epoch < self.lr_ramp_ep + self.lr_sus_ep:
+            lr = self.lr_max
+            
+        else:
+            lr = ((self.lr_max - self.lr_min) * self.lr_decay**
+                  (self.last_epoch - self.lr_ramp_ep - self.lr_sus_ep) + 
+                  self.lr_min)
+        return lr
+
+### GradualWarmupScheduler
+# https://github.com/ildoonet/pytorch-gradual-warmup-lr
+
+class GradualWarmupScheduler(_LRScheduler):
+    """ Gradually warm-up(increasing) learning rate in optimizer.
+    Proposed in 'Accurate, Large Minibatch SGD: Training ImageNet in 1 Hour'.
+    Args:
+        optimizer (Optimizer): Wrapped optimizer.
+        multiplier: target learning rate = base lr * multiplier if multiplier > 1.0. if multiplier = 1.0, lr starts from 0 and ends up with the base_lr.
+        total_epoch: target learning rate is reached at total_epoch, gradually
+        after_scheduler: after target_epoch, use this scheduler(eg. ReduceLROnPlateau)
+    """
+
+    def __init__(self, optimizer, multiplier, total_epoch, after_scheduler=None):
+        self.multiplier = multiplier
+        if self.multiplier < 1.:
+            raise ValueError('multiplier should be greater thant or equal to 1.')
+        self.total_epoch = total_epoch
+        self.after_scheduler = after_scheduler
+        self.finished = False
+        super(GradualWarmupScheduler, self).__init__(optimizer)
+
+    def get_lr(self):
+        if self.last_epoch > self.total_epoch:
+            if self.after_scheduler:
+                if not self.finished:
+                    self.after_scheduler.base_lrs = [base_lr * self.multiplier for base_lr in self.base_lrs]
+                    self.finished = True
+                return self.after_scheduler.get_last_lr()
+            return [base_lr * self.multiplier for base_lr in self.base_lrs]
+
+        if self.multiplier == 1.0:
+            return [base_lr * (float(self.last_epoch) / self.total_epoch) for base_lr in self.base_lrs]
+        else:
+            return [base_lr * ((self.multiplier - 1.) * self.last_epoch / self.total_epoch + 1.) for base_lr in self.base_lrs]
+
+    def step_ReduceLROnPlateau(self, metrics, epoch=None):
+        if epoch is None:
+            epoch = self.last_epoch + 1
+        self.last_epoch = epoch if epoch != 0 else 1  # ReduceLROnPlateau is called at the end of epoch, whereas others are called at beginning
+        if self.last_epoch <= self.total_epoch:
+            warmup_lr = [base_lr * ((self.multiplier - 1.) * self.last_epoch / self.total_epoch + 1.) for base_lr in self.base_lrs]
+            for param_group, lr in zip(self.optimizer.param_groups, warmup_lr):
+                param_group['lr'] = lr
+        else:
+            if epoch is None:
+                self.after_scheduler.step(metrics, None)
+            else:
+                self.after_scheduler.step(metrics, epoch - self.total_epoch)
+
+    def step(self, epoch=None, metrics=None):
+        if type(self.after_scheduler) != ReduceLROnPlateau:
+            if self.finished and self.after_scheduler:
+                if epoch is None:
+                    self.after_scheduler.step(None)
+                else:
+                    self.after_scheduler.step(epoch - self.total_epoch)
+                self._last_lr = self.after_scheduler.get_last_lr()
+            else:
+                return super(GradualWarmupScheduler, self).step(epoch)
+        else:
+            self.step_ReduceLROnPlateau(metrics, epoch)
+
+### GradualWarmupSchedulerV2
+
+class GradualWarmupSchedulerV2(GradualWarmupScheduler):
+    def __init__(self, optimizer, multiplier, total_epoch, after_scheduler=None):
+        super(GradualWarmupSchedulerV2, self).__init__(optimizer, multiplier, total_epoch, after_scheduler)
+
+    def get_lr(self):
+        if self.last_epoch > self.total_epoch:
+            if self.after_scheduler:
+                if not self.finished:
+                    self.after_scheduler.base_lrs = [base_lr * self.multiplier for base_lr in self.base_lrs]
+                    self.finished = True
+                return self.after_scheduler.get_lr()
+            return [base_lr * self.multiplier for base_lr in self.base_lrs]
+            
+        if self.multiplier == 1.0:
+            return [base_lr * (float(self.last_epoch) / self.total_epoch) for base_lr in self.base_lrs]
+        else:
+            return [base_lr * ((self.multiplier - 1.) * self.last_epoch / self.total_epoch + 1.) for base_lr in self.base_lrs]
---
+++ input/utils/get_KNN.py
@@ -0,0 +1,33 @@
+import gc
+
+import numpy as np
+import pandas as pd
+
+# from sklearn.neighbors import NearestNeighbors
+import cuml, cupy
+
+import  eval_preds
+
+def get_valid_neighbors(df, embeddings, destination, threshold, KNN = 55):
+    print(f"Finding similar by cosine KNN..., len of train: {len(df)}, KNN={KNN}")
+    model = cuml.NearestNeighbors(n_neighbors = KNN, metric = 'cosine')
+    model.fit(embeddings)
+    distances, indices = model.kneighbors(embeddings)
+
+    predictions = []
+    for k in range(embeddings.shape[0]):
+        idx = np.where(distances[k,] < threshold)[0]
+        ids = indices[k,idx]
+        posting_ids = np.array(df['posting_id'].iloc[ids].values)
+        # predictions.append(posting_ids)
+        predictions.append(' '.join(posting_ids))
+    
+    df[destination] = predictions
+
+    df['precision'],df['recall'],df['f1'] =  eval_preds.get_score(df['matches'], df[destination])
+    # if COMPUTE_CV:
+        # df['precision'],df['recall'],df['f1'] = get_score(df['matches'], df[destination])
+
+    del model, distances, indices
+    gc.collect()
+    return df
